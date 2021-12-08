77d1b21ed468e812bcb9f5460c0ce450cc2e02ec
Engdahlt <engdahl.tobias@gmail.com>
2021-12-08 21:48:12 +0100

    Tagit väck kommentarer..
    Tagit väck kommentarer och lektionsanteckningar jag använt mig av. Samt fixat till sista för att få den sparade datan i JSON-filen skrivas ut på html-sidan.

--- Publik/javascript/klientskript.js
+++ Publik/javascript/klientskript.js
@@ -1,5 +1,23 @@
-//skapat början av mitt klientskript 
-
+//Klientskript
 window.onload = () => {
-
+    const output = document.getElementById("output")
+    let getMsgData = function () {
+        let dataRequest = new XMLHttpRequest();
+        dataRequest.open("GET", "/messages");
+        dataRequest.onload = function () {
+            
+    const data = JSON.parse(this.response);
+    console.log(data)
+    data.gastboksinlagg.map(msg => {
+    output.innerHTML += `
+                  <td>${msg.namn}</td>           
+                  <td>${msg.epost}</td>         
+                  <td>${msg.amne}</td>          
+                  <td>${msg.inlagg}</td>            
+                  `
+            })
+        }
+        dataRequest.send();                       
+    }
+    getMsgData()
 }
